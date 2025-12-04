35cabcd53152054e7b844f5644c713443f40d1dd
El Miri Martin <elmiri.martin@gmail.com>
2025-12-04 14:35:47 +0100

    kész az egész

--- controller.py
+++ controller.py
@@ -1,19 +1,34 @@
 from fileManager import FileManager
 
-class WorkerManager:
+class WorkerManager(FileManager):
 
     def __init__(self):
         self.workerList = []
 
     def controller(self):
-        self.workerList = FileManager.betolt_alkalmazottakat("alkalmazottak.txt")
-        
-        name, salary = self.legnagyobb_fizetett()
-        print(f"Legjobban fizetett ember: {name} fizetése: {salary}")
+        self.workerList = self.betolt_alkalmazottakat("alkalmazottak.txt")
+
+        self.addWorker()
+
+        data = self.legnagyobb_fizetett()
+        name = self.workerList[self.workerList.index(data)][1]
+        salary = self.workerList[self.workerList.index(data)][2]
+print(f"Legjobban fizetett ember: {name} fizetése: {salary}")
+
+        self.ment_alkalmazottakat(self.workerList, "teszt.txt")
+
+    def addWorker(self):
+        worker = ['1016','János Jani','450000','Fejlesztő']
+        self.workerList.append(worker)
+        print(self.workerList)
 
     def legnagyobb_fizetett(self):
         highest_worker = self.workerList[0]
         for worker in self.workerList:
-            if int(worker.fizetes) > int(highest_worker.fizetes):
+            if int(worker[2]) > int(highest_worker[2]):
                 highest_worker = worker
-        return highest_worker.nev, highest_worker.fizetes
+        return highest_worker
+
+
+wm = WorkerManager()
+wm.controller()
